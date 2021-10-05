44083bc8052c6a89ade3fcf222949a3d43787959
LeidyJRuiz <leidy.ruiz.0ds@utap.edu.co>
2021-10-04 21:22:49 -0500

    CLASE 2021-10-04

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
+
+import {HttpClientModule } from '@angular/common/http';
+
+import {StudentService} from './service/student.service';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentListComponent } from './component/student-list/student-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    StudentService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
