23a57b26dfe81348b425437740eb02df67660df7
LeidyJRuiz <leidy.ruiz.0ds@utap.edu.co>
2021-10-11 12:54:25 -0500

    CLASE 2021-10-06

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import {HttpClientModule } from '@angular/common/http';
+import{FormsModule}from '@angular/forms';
 
 import {StudentService} from './service/student.service';
 
@@ -9,16 +10,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentListComponent } from './component/student-list/student-list.component';
+import { StudentCreateComponent } from './component/student-create/student-create.component';
+import { StudentEditComponent } from './component/student-edit/student-edit.component';
+import { StudentDeleteComponent } from './component/student-delete/student-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentListComponent
+    StudentListComponent,
+    StudentCreateComponent,
+    StudentEditComponent,
+    StudentDeleteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     StudentService
